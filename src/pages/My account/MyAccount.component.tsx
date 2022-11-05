f4f81242535d3e3c6826313edc84d729e8ac4512
LightBlue44 <thalesgirotto@gmail.com>
2022-11-05 11:28:29 -0300

    try fixing

--- src/pages/My account/MyAccount.component.tsx
+++ src/pages/My account/MyAccount.component.tsx
@@ -12,40 +12,37 @@ import Paper from '../../components/atoms/Paper'
 import Typography from '../../components/atoms/Typography'
 import { useUser } from '../../context/User'
 import { useFormats } from '../../utils/useFormats'
+import { getAll } from '../../services/Product'
+import { Row } from 'react-grid-system'
 
 const MyAccount = () => {
     const {dispatch} = useUser()
     const {formatCurrency, getFisrtName} = useFormats()
     const [purchase] = useState<PurchaseInterface[]>([])
   
-    const payment = (purchase: string) => {
+    const payment => {
       UserService.(purchase)
-      .then(() => return payment 
-    }
+    } .then(() => return payment 
+
 
       (
       <Container 
         fullHeight
         fullCentered
       >
-        <Typography>Olá {getFisrtName(state.user?.name)}</Typography>
+        <Typography>
+          Olá {getFisrtName(state.user?.name)} <br />
+          
+        </Typography>
         <Row>
-          {purchase.map(purchase) => (
+          {purchase.map(purchase) ={'>'} (
           <Paper>
-            <Typography>Produto 01</Typography>
-            <Button>Pagar</Button> <br />
-
-            <Typography>Produto 02</Typography>
-            <Button>Pagar</Button> <br />
-
-            <Typography>Produto 03</Typography>
-            <Button>Pagar</Button> <br />
-
-           <Typography>Produto 04</Typography>
-            <Button>Pagar</Button> <br />
-
-            <Typography>Produto 05</Typography>
-            <Button>Pagar</Button> <br />
+            <Typography>Produto 01</Typography><br />
+            <Typography>Produto 02</Typography><br />
+            <Typography>Produto 03</Typography><br />
+            <Typography>Produto 04</Typography><br />
+            <Typography>Produto 05</Typography><br />
+            <Button onClick={getAll}>Pagar</Button>
           </Paper>
           )}
         </Row>
